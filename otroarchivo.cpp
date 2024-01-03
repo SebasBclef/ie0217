8df342ca3f6626fd50bcf42f2153286a5e10245f
Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>
2024-01-03 14:52:24 -0600

    Agregando el otroarchivo.cpp
    Signed-off-by: Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>

---
+++ otroarchivo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+//este es el otro archivo.
+using namespace std;
+int main() 
+{
+    std::cout << "Hello World. Its sad being a computer :(. But from another file :D";
+    return 0;
+}
