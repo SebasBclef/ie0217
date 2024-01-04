5e8bb9fbf83fa7a2e6a13258d467d5ae944abedf
Sebastian Bonilla <135726555+SebasBclef@users.noreply.github.com>
2024-01-04 12:33:43 -0600

    Delete otroarchivo.cpp

--- otroarchivo.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-//este es el otro archivo.
-using namespace std;
-int main() 
-{
-    std::cout << "Hello World. Its sad being a computer :(. But from another file :D\n";
-    std::cout << "Esta es otra linea de codigo. :D";
-    return 0;
-}
