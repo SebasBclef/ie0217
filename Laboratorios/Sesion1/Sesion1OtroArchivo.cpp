0bb19f052dc80791ad35775af40f29c8d4de3d92
Sebastian Bonilla <135726555+SebasBclef@users.noreply.github.com>
2024-01-04 13:00:39 -0600

    Create Sesion1OtroArchivo.cpp

---
+++ Laboratorios/Sesion1/Sesion1OtroArchivo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//este es el otro archivo.
+using namespace std;
+int main() 
+{
+    std::cout << "Hello World. Its sad being a computer :(. But from another file :D\n";
+    std::cout << "Esta es otra linea de codigo. :D";
+    return 0;
+}
