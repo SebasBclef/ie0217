723accaf0cf0ec8a19484c2ab9edfb3e6709d40a
Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>
2024-01-05 18:12:25 -0600

    Anadiendo Laboratorio 1, fecha: 5/1/24
    Signed-off-by: Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>

---
+++ Laboratorios/Laboratorio 5-1-24/funciones.hpp
@@ -0,0 +1,20 @@
+#ifndef FUNCIONES_HPP
+#define FUNCIONES_HPP
+#include <string>
+#include <iostream>
+
+const int MAX_EMPLEADOS=10;
+ 
+struct Empleado {
+    int id;
+    std :: string nombre;
+    double salario;
+};
+
+void MostrarMenu();
+void procesarOpcion (Empleado empleados[], int& numEmpleados);
+void agregarEmpleado (Empleado empleados[], int& numEmpleados);
+void listarEmpleados (const Empleado empleados[], int& numEmpleados);
+void eliminarEmpleado (Empleado empleados[], int& numEmpleados);
+
+#endif
