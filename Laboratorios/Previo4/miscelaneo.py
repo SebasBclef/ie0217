100e789cba626754e834ab58afcc73762b190fa9
Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>
2024-01-19 12:55:43 -0600

    Agregando la sesion
    Signed-off-by: Sebastian Bonilla <sebastian.bonillavega@ucr.ac.cr>

---
+++ Laboratorios/Previo4/miscelaneo.py
@@ -0,0 +1,21 @@
+import constant
+
+pagina="eie.ucr.ac.cr"
+print(pagina)
+
+pagina="Google"
+
+print (pagina)
+
+a,b,c= 5,3.2, "Holi"
+print(a) ##imprime a
+print(b) ##imprime b
+print(c) ##imprime c
+
+
+print(constant.PI)
+print(constant.Euler)
+print(constant.ELDIABLO)
+print(constant.Futurama)
+print(constant.NICE)
+print(constant.Gravedad)
